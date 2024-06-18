d0eb152240a6d0caff1a756e1da710583b9af5c0
joao-pcd <jclementinodias@gmail.com>
2024-06-18 14:23:48 -0300

    Troca do swagger para uma versão compatível ao Spring Boot

--- src/main/java/com/joaopcd/design_patterns/Application.java
+++ src/main/java/com/joaopcd/design_patterns/Application.java
@@ -3,9 +3,11 @@ package com.joaopcd.design_patterns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = "com.joaopcd.design_patterns")
 public class Application {
 
 	public static void main(String[] args) {
